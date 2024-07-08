6b03a45e568787cae46b0b3539d967edef85f008
Sourabh Sejwal <sourabh.sejwal@scryai.com>
2024-07-08 18:04:41 +0530

    sdfafd

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import argparse
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import itertools
+# import itertools
 
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
